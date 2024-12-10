4f8c39fb9efa66c8a17e394e9670c70223f9516f
Jalil Abdullayev <abdullacelil00@gmail.com>
2024-12-10 13:10:59 +0400

    feat: create Header component
    - remove unnecessary components & assets
    
    BREAKING CHANGE: Removed previous app structure.  Refactored to new component structure.

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      Task Tracker
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
